3c4a62de53353ef1e0efca0bbc3760ffafcd0476
JMLI <JMLI@MCK790LJAYMLI>
2022-01-24 19:29:35 -0600

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,63 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div class="container">
+    <b-navbar :fixed-top="true" class="topbar">
+    <template slot="brand">
+        <img src="~@/assets/images/special-risk-logo.png" class="logo" />
+    </template>
+        <template slot="end">
+            <b-navbar-item href="#">
+                Submit Activity
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Quick Quote
+            </b-navbar-item>
+            <b-navbar-item tag="router-link" :to="'admin'">
+                Admin
+            </b-navbar-item>
+        </template>
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons" style="margin-right: 20px;">
+                    <a class="button is-success"><strong>Login</strong></a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+    <div class="router-view">
+      <router-view/>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.navbar.is-fixed-top {
+  background: #2C863A;
 }
-
-#nav {
-  padding: 30px;
+a.navbar-item, .navbar-link {
+  color: white;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+a.nav-link {
+  color: white;
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+a.nav-link:hover {
+  color: #7957d5;
+}
+.navbar-menu.is-active {
+  background: bottom;
+}
+.logo {
+  padding: 2px;
+  margin-left: 10px;
+}
+.router-view {
+  margin-top: 20px;
 }
 </style>
---
+++ src/views/Admin.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="admin">
+      This is admin
+  </div>
+</template>
