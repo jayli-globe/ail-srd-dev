1d9f9bbbc7538f477a48225bab82de0d7a549bdf
JMLI <JMLI@MCK790LJAYMLI>
2022-01-24 19:38:18 -0600

    cleanup adn padding

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="container">
     <b-navbar :fixed-top="true" class="topbar">
-    <template slot="brand">
-        <img src="~@/assets/images/special-risk-logo.png" class="logo" />
-    </template>
-        <template slot="end">
-            <b-navbar-item href="#">
-                Submit Activity
-            </b-navbar-item>
-            <b-navbar-item href="#">
-                Quick Quote
-            </b-navbar-item>
-            <b-navbar-item tag="router-link" :to="'admin'">
-                Admin
-            </b-navbar-item>
-        </template>
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <div class="buttons" style="margin-right: 20px;">
-                    <a class="button is-success"><strong>Login</strong></a>
-                </div>
-            </b-navbar-item>
-        </template>
+      <template slot="brand">
+          <img src="~@/assets/images/special-risk-logo.png" class="logo" />
+      </template>
+      <template slot="end">
+          <b-navbar-item href="#">
+              Submit Activity
+          </b-navbar-item>
+          <b-navbar-item href="#">
+              Quick Quote
+          </b-navbar-item>
+          <b-navbar-item tag="router-link" :to="'admin'">
+              Admin
+          </b-navbar-item>
+      </template>
+      <template slot="end">
+          <b-navbar-item tag="div">
+              <div class="buttons" style="margin-right: 20px;">
+                  <a class="button is-success"><strong>Login</strong></a>
+              </div>
+          </b-navbar-item>
+      </template>
     </b-navbar>
     <div class="router-view">
       <router-view/>
@@ -38,17 +38,14 @@ export default {
 </script>
 
 <style>
+.topbar {
+  padding: 2px;
+}
 .navbar.is-fixed-top {
   background: #2C863A;
 }
 a.navbar-item, .navbar-link {
   color: white;
-}
-a.nav-link {
-  color: white;
-}
-a.nav-link:hover {
-  color: #7957d5;
 }
 .navbar-menu.is-active {
   background: bottom;
@@ -58,6 +55,6 @@ a.nav-link:hover {
   margin-left: 10px;
 }
 .router-view {
-  margin-top: 20px;
+  margin-top: 25px;
 }
 </style>
